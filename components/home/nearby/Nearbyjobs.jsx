8da42b9ee0f43db741de9adb69c85be65afc98b3
allanviguilla <viguilla.allan@gmail.com>
2023-04-19 09:40:44 -0700

    Debug broken images on NearbyJobs component

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -14,31 +14,31 @@ export default function Nearbyjobs() {
   const router = useRouter();
 
   const { data, isLoading, error } = useFetch("search", {
-    query: "React Developer",
-    num_pages: 1,
+    query: "React Native Developer",
+    num_pages: "1",
   });
 
-  // console.log(data);
+  //console.log(data);
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Nearby Jobs</Text>
         <TouchableOpacity>
-          <Text style={styles.headerBtn}>Show all</Text>
+          <Text style={styles.headerBtn}>Show All</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.cardsContainer}>
         {isLoading ? (
-          <ActivityIndicator size="large" colors={COLORS.primary} />
+          <ActivityIndicator size="large" color={COLORS.primary} />
         ) : error ? (
-          <Text>Something went wrong.</Text>
+          <Text>Something went wrong</Text>
         ) : (
           data?.map((job) => (
             <NearbyJobCard
               job={job}
-              key={`nearby-job-${job?.job_id}`}
+              key={`nearby-job-${job.job_id}`}
               handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
             />
           ))
